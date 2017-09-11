c69ce0cbaa4d9b062cb64fa9faf8e48a8d7a2a91
carlogeertse <carlogeertse@hotmail.com>
2017-09-11 14:52:37 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/workshops_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/workshops_c.dir/main.c.obj"
+  "workshops_c.pdb"
+  "workshops_c.exe"
+  "workshops_c.exe.manifest"
+  "libworkshops_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/workshops_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/workshops_c.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for workshops_c.
+# This may be replaced when dependencies are built.
