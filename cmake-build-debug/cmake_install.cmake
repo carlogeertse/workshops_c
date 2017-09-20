a0584b95fc27bbcd8112541242f900f222517a5e
carlogeertse <carlogeertse@hotmail.com>
2017-09-20 16:36:22 +0200

    Progress exercise 1-1-5

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Carlo/CLionProjects/workshops_c
+# Install script for directory: D:/C_workspace/workshops_c
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Carlo/CLionProjects/workshops_c/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/C_workspace/workshops_c/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- workshop1/exercise1/exercise1-1-5.c
+++ workshop1/exercise1/exercise1-1-5.c
@@ -3,6 +3,19 @@
 int main() {
     int amount = 0; //init aray size 1
 
+    /**
+     * Moves some items in an array to the right
+     * @param int items[]: An array filled with items
+     * @param int shiftStart: The index of the start of the collection of items that need to be shifted to the right
+     * @param int shiftEnd: The index of the end of the collection of items that need to be shifted to the right
+     * @param int shiftAmount: The amount of positions that the items need to be shifted to the right
+     */
+    void shiftRight(int items[], int shiftStart, int shiftEnd, int shiftAmount) {
+        for (int i = shiftEnd; i > shiftStart; i--) {
+            items[i] = items[i - shiftAmount];
+        }
+    }
+
     //Fill array 1
     printf("Please insert the amount of integers to be stored in the arrays:\n");
     scanf("%d", &amount);
@@ -20,11 +33,30 @@ int main() {
         scanf("%d", &numberArray2[i]);
     }
 
-    int mergedArray[amount*2];
+    int mergedArray[amount * 2];
 
-    
+    //Copy firt element of array 1 into merged array
+    mergedArray[0] = numberArray1[0];
 
-    void swapArrayElements(int intArray[], int index1, int index2){
+    int insterted = 0;
+    int i = 0;
+    //Merge the rest of array1 into merged array, sorting in the meantime
+    for (int j = 1; j < amount; ++j) {
+        while (!insterted) {
+            if (numberArray1[j] < mergedArray[i]) {
+                shiftRight(mergedArray, i, amount * 2 - 1, 1);
+                mergedArray[i] = numberArray1[j];
+                insterted = 1;
+            } else if (i == amount * 2 - 1) {
+                mergedArray[i] = numberArray1[j];
+            } else {
+                i++;
+            }
+        }
+    }
 
+
+    for (int j = 0; j < 3; j++) {
+        printf("%d ", mergedArray[j]);
     }
 }
